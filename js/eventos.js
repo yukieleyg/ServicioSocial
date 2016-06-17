1ffc8e85ef29db3f248f4d7fcb266938d53c3159
Yukie Ley <cylg93@hotmail.com>
2016-06-17 15:59:28 -0600

    Segundo commit

---
+++ js/eventos.js
@@ -0,0 +1,38 @@
+ var inicio = function(){
+	var parametros="";
+	var usuario="";
+	var clave="";
+
+	var entrar = function(){
+		debugger
+
+		usuario = $("#txtUsuario").val();
+		clave   = $("#txtClave").val();
+
+		if(docente=="" || clave==""){
+			alert("Complete los campos para acceder");
+		}else{
+			$.ajax({
+				type: "GET",
+				dataType: "json",
+				url: "php/utilerias.php",
+				data: parametros,
+
+				success: function(data){
+
+					if(data.respuesta){
+						$(".entradaUsuario").hide("slow");
+						$("#barra").show("slow");
+						$("#user").append("<i class='material-icons'>perm_identity</i>"+usuario+"<br><br>");
+						$("#user").show("slow");
+					}else{
+						alert("Usuario no registrado");
+					}
+				}
+			});
+		}
+		
+	}
+
+
+$(document).on("ready",inicio);
